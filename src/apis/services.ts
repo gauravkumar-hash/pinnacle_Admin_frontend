5526a642aa18384de832d7766a87b1ca46d5687e
gauravkumar-hash <gauravkumar@singsys.com>
2026-07-21 15:09:43 +0530

    Rename clinic_photo_path to clinic_logo_path

--- src/apis/services.ts
+++ src/apis/services.ts
@@ -26,7 +26,7 @@ export interface Service {
   service_name: string;
   clinic_name: string;
   consultation_fee?: string;
-  clinic_photo_path?: string;
+  clinic_logo_path?: string;
   banner_image_path?: string;
   bio?: string;
   service_details?: string;
